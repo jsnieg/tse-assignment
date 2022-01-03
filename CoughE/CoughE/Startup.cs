9c305899d18cdcb47f01d27c829c35607eba2147
Tom Brayshaw <tombrayshaw@icloud.com>
2022-01-03 13:35:39 +0000

    no need for endpoints yet

--- CoughE/CoughE/Startup.cs
+++ CoughE/CoughE/Startup.cs
@@ -50,12 +50,12 @@ namespace CoughE
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller}/{action=Index}/{id?}");
-            });
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllerRoute(
+            //        name: "default",
+            //        pattern: "{controller}/{action=Index}/{id?}");
+            //});
 
             app.UseSpa(spa =>
             {
