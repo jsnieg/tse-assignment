eabcf89fc11a6b1bfdadbe3f811cc7be28696f4a
Tom Brayshaw <tombrayshaw@icloud.com>
2022-01-31 13:13:55 +0000

    add recording component

---
+++ KOUGH/KOUGH/ClientApp/src/components/AudioRecorder/AudioRecorder.jsx
@@ -0,0 +1,34 @@
+﻿import React, { Component } from 'react';
+import { Recorder } from 'react-voice-recorder';
+import 'react-voice-recorder/dist/index.css';
+
+export class AudioRecorder extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            audioDetails: {
+                url: null,
+                blob: null,
+                chunks: null,
+                duration: {
+                    h: 0,
+                    m: 0,
+                    s: 0
+                }
+            }
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <Recorder
+                    record={true}
+                    title={"Record your voice"}
+                    showUIAudio
+                    mimeTypeToUseWhenRecording={`audio/webm`}
+                />
+            </div>
+        );
+    };
+};
