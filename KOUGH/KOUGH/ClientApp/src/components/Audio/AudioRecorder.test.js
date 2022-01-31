89b04b900ef5f8e49df21bf86b0bb83b9bb4d557
Tom Brayshaw <tombrayshaw@icloud.com>
2022-01-31 18:03:00 +0000

    add test file

---
+++ KOUGH/KOUGH/ClientApp/src/components/Audio/AudioRecorder.test.js
@@ -0,0 +1,10 @@
+﻿import React from 'react';
+import {render} from '@testing-library/react';
+
+it('renders without crashing', async () => {
+    const div = document.createElement('div');
+    
+  });
+  
+
+
