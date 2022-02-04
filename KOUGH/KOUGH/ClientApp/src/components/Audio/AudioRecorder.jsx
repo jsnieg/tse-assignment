1c2a798952e6e44f40cee3770aea13b5f8786dc0
Tom Brayshaw <tombrayshaw@icloud.com>
2022-02-04 18:33:09 +0000

    refactor

--- KOUGH/KOUGH/ClientApp/src/components/Audio/AudioRecorder.jsx
+++ KOUGH/KOUGH/ClientApp/src/components/Audio/AudioRecorder.jsx
@@ -9,48 +9,51 @@ export default function AudioRecorder({ recorderState, handlers }) {
     return (
         <div className="controls-container">
             <div className="recorder-display">
-            <div className="recording-time">
-                { initRecording && 
+                <div className="recording-time">
+                {
+                    initRecording &&
                     <div className="recording-indicator"></div>
                 }
 
-                <span> { 
-                    recordingMinutes < 10 ? `0${recordingMinutes}` : recordingMinutes 
-                } </span>
+                <span> { recordingMinutes < 10 ? `0${recordingMinutes}` : recordingMinutes } </span>
                 <span>:</span>
-                <span> { 
-                    recordingSeconds < 10 ? `0${recordingSeconds}` : recordingSeconds 
-                } </span>
+                <span> { recordingSeconds < 10 ? `0${recordingSeconds}` : recordingSeconds } </span>
                 
                 </div>
+            </div>
+
+            <div className="control-button-container">
                 { initRecording && (
                     <div className="cancel-button-container">
-                        <button 
-                            className="btn-primary cancel-button" 
-                            title="Cancel recording" 
+                        <button
+                            className="cancel-button"
+                            title="Cancel recording"
                             onClick={cancelRecording}
-                        ><i class="fas fa-ban" size='2x'></i>
+                        ><i class="fas fa-trash"></i>
                         </button>
                     </div>
                 )}
-            </div>
-
-            <div className="start-button-container">
+                
                 { initRecording ? (
-                    <button
-                        className="btn-primary start-button"
-                        title="Save recording"
-                        disabled={recordingSeconds === 0}
-                        onClick={saveRecording}
-                    ><i class="far fa-save" size='2x'></i>
-                    </button>
+                    <div className="save-button-container">
+                        <button
+                            className="btn-primary start-button"
+                            title="Save recording"
+                            disabled={recordingSeconds === 0}
+                            onClick={saveRecording}
+                        ><i class="far fa-save"></i>
+                        </button>
+                    </div>
+                    
                     ) : (
-                    <button 
-                        className="btn-primary start-button" 
-                        title="Start recording" 
-                        onClick={startRecording}
-                    ><i class="fas fa-microphone" size='2x'></i>
-                    </button> 
+                    <div className="start-button-container">
+                        <button
+                            className="btn-primary start-button"
+                            title="Start recording"
+                            onClick={startRecording}
+                        ><i class="fas fa-microphone"></i>
+                        </button>
+                    </div>
                     )
                 }
             </div>
