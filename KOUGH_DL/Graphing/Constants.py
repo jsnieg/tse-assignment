1c66a43a630423397f03d963a8b3dfb8435f4b20
Tom Brayshaw <tombrayshaw@icloud.com>
2022-02-21 15:58:16 +0000

    add spectrogram graphing

--- KOUGH_DL/Graphing/Constants.py
+++ KOUGH_DL/Graphing/Constants.py
@@ -6,3 +6,9 @@ COVID_POS_SIGNAL, COVID_POS_SR = librosa.load(COVID_POSITIVE_PATH, sr=22050)
 
 COVID_NEGATIVE_PATH = "../Train/covid-negative/covid-negative.wav"
 COVID_NEG_SIGNAL, COVID_NEG_SR = librosa.load(COVID_NEGATIVE_PATH, sr=22050)
+
+# The window to perform a stft per FFT
+N_SAMPLES_PER_FFT = 2048
+
+# The amount we are tranfroming each FFT to the right
+HOP_LENGTH = 512
