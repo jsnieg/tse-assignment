09e46f6a5326b179661d93bf0cf3d18dd18732be
Tom Brayshaw <tombrayshaw@icloud.com>
2022-03-18 11:40:39 +0000

    update and testing pre-processing stuff for image classification AI

--- KOUGH_DL/Graphing/Spectrogram.py
+++ KOUGH_DL/Graphing/Spectrogram.py
@@ -1,20 +1,18 @@
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 import numpy as np
 import librosa
 import librosa.display
 import Constants as const
 
 
-def DisplaySpectrogram(log_spectrogram, sr, hop_length):
+def DisplaySpectrogram(filename, log_spectrogram, sr, hop_length):
     librosa.display.specshow(
         log_spectrogram,
         sr=sr,
         hop_length=hop_length
     )
-    matplotlib.pyplot.xlabel("Time (s)")
-    matplotlib.pyplot.ylabel("Frequency (db)")
-    matplotlib.pyplot.colorbar()
-    matplotlib.pyplot.show()
+    plt.savefig(f'{filename}.png', format='png')
+    plt.close()
 
 
 def STFT(signal, hop_length, n_samples):
@@ -36,6 +34,7 @@ log_spectrogram = librosa.amplitude_to_db(
 )
 
 DisplaySpectrogram(
+    'covid-neg',
     log_spectrogram,
     const.COVID_NEG_SR,
     const.HOP_LENGTH
@@ -54,6 +53,7 @@ log_spectrogram = librosa.amplitude_to_db(
 )
 
 DisplaySpectrogram(
+    'covid-pos',
     log_spectrogram,
     const.COVID_POS_SR,
     const.HOP_LENGTH
