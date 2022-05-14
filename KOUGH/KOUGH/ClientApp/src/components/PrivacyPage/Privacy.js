66cb3dd6db16f2e546c4b43b8d38f13c01d96c71
Tom Brayshaw <48366715+t0mB1@users.noreply.github.com>
2022-05-14 12:24:06 +0100

    add Privacy page

---
+++ KOUGH/KOUGH/ClientApp/src/components/PrivacyPage/Privacy.js
@@ -0,0 +1,20 @@
+﻿import React from 'react';
+import SinglePagePreviewPDF from '../PDF/SinglePage';
+import samplePDF from '../../sample.pdf';
+
+
+const Privacy = () => {
+  return (
+    <div>
+      <h1> Privacy Disclaimer </h1>
+
+      <section id='pdf-preview-section'>
+        <SinglePagePreviewPDF 
+          pdf={samplePDF}
+        />
+      </section>
+    </div>
+  );
+}
+
+export default Privacy;
